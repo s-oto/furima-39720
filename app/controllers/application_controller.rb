bb73f221593c8d45e28ab77e691abde27c9af559
s-oto <s.oto.stohi@gmail.com>
2023-12-21 15:07:16 +0900

    一旦コミット

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :last_name, :last_name_read, :first_name, :first_name_read, :birthday])
+  end
 end
