659042d33042976dfad721453bd79aa3725961d9
s-oto <s.oto.stohi@gmail.com>
2023-12-30 15:59:57 +0900

    正常系のテストコード追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    item {"あ"}
+    description {"あ"}
+    price {300}
+    category_id {2}
+    status_id {2}
+    charge_id {2}
+    day_id {2}
+    prefecture_id {2}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
